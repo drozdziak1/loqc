52f852c2a7e4e89b8c4b2750d5699e5456ea6c45
Stan Drozd <drozdziak1@gmail.com>
2021-10-18 19:57:46 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "LOw-effort Question Contraption";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/release-21.05;
+    rust-overlay.url = github:oxalica/rust-overlay;
+    flake-utils.url = github:numtide/flake-utils;
+    cargo2nix = {
+      url = github:cargo2nix/cargo2nix;
+      flake = false;
+    };
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, cargo2nix }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          c2n-olay = import "${cargo2nix}/overlay";
+          pkgs = import nixpkgs {
+            overlays = [ rust-overlay.overlay c2n-olay ];
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+          rec {
+            devShell = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [ #(callPackage cargo2nix {}).package
+                                               rust-bin.stable."1.55.0" ];
+            };
+            packages = rec {};
+          }
+    );
+
+}
